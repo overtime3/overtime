294b44ccf925fa30e10f3fa375a9c8766b47f3ea
dongyan1024 <39162246+dongyan1024@users.noreply.github.com>
2021-09-27 21:25:22 +0800

    Update __init__.py

--- overtime/algorithms/__init__.py
+++ overtime/algorithms/__init__.py
@@ -1,4 +1,3 @@
 from overtime.algorithms.foremost import *
 from overtime.algorithms.reachability import *
 from overtime.algorithms.edgeDeletion import *
-from overtime.algorithms.sliding_window_temporal_vertex_cover import *
